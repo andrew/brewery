cc9e227e74b05c3452ebac04c740f11eb75252cd
Andrew Nesbitt <andrewnez@gmail.com>
2011-03-29 13:47:05 +0100

    Initial Commit

---
+++ Brewery/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Brewery
+#
+#  Created by Andrew Nesbitt on 29/03/2011.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ Brewery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Brewery
+//
+//  Created by Andrew Nesbitt on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
