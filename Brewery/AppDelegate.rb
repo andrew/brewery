9c3d1a0efdfdf5d5281b105c0941ff8b452dfd55
Andrew Nesbitt <andrewnez@gmail.com>
2011-06-02 13:03:54 +0100

    Actually do something related to homebrew!
    Shows the names and versions of all software installed by homebrew

--- Brewery/AppDelegate.rb
+++ Brewery/AppDelegate.rb
@@ -6,10 +6,43 @@
 #  Copyright 2011 __MyCompanyName__. All rights reserved.
 #
 
+HOMEBREW_BREW_FILE = ENV['HOMEBREW_BREW_FILE'] = '/usr/local/bin/brew'
+
+require 'pathname'
+HOMEBREW_LIBRARY_PATH = Pathname.new('/usr/local/bin/brew').realpath.dirname.parent.join("Library/Homebrew").to_s
+
+$:.unshift(HOMEBREW_LIBRARY_PATH)
+require 'global'
+
+
 class AppDelegate
     attr_accessor :window
+    attr_accessor :kegs, :keg_table
+
     def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+        @kegs = HOMEBREW_CELLAR.children.select{ |pn| pn.directory? }
+
+        keg_table.dataSource = self
+        keg_table.doubleAction = "preview:"
+    end
+
+    def windowWillClose(sender);
+        exit;
+    end
+
+    def numberOfRowsInTableView(view)
+        kegs.size
+    end
+
+    def tableView(view, objectValueForTableColumn:column, row:index)
+        keg = kegs[index]
+        id = column.identifier
+        case id
+        when 'name'
+            keg.basename.to_s
+        when 'version'
+            keg.children.select{ |pn| pn.directory? }.map{ |pn| pn.basename.to_s }.join(', ')
+        end
     end
 end
 
